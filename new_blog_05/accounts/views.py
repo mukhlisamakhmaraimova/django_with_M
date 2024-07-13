5ff96e8ac637140cac4d52155bc4a5a9249b5c08
mukhlisamakhmaraimova <makhmaraimovamukhlisa77@gmail.com>
2024-07-13 23:42:38 +0500

    adding address in views.py

--- new_blog_05/accounts/views.py
+++ new_blog_05/accounts/views.py
@@ -4,10 +4,16 @@ from django.shortcuts import render
 
 
 class Accounts:
-    def __init__(self, name, age):
+    def __init__(self, name, age, address):
         self.name = name
         self.age = age
+        self.address = address
 
     def get_info(self):
         return f'My name is {self.name} \n I\'m {self.age}'
+
+    def get_address(self):
+        return f"My address: {self.address}"
+
+
 
